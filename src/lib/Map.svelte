71a7cdad59a95d067bff9f2ae8b99aa2c1dbdb09
romanoe <noemi.romano@protonmail.com>
2023-03-25 16:31:36 +0100

    change color scheme

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -12,8 +12,6 @@
     import {Grid, Column, Row} from "carbon-components-svelte";
 
 
-
-
     let geodata = [], projection, path, colorScaleMariage, colorScaleConversions;
     let selected = 'total';
 
@@ -24,8 +22,8 @@
 
     // SVG properties
     const margin = {top: 50, right: 50, bottom: 0, left: 50},
-        w = window.innerWidth/1.5 - margin.left - margin.right,
-        h = window.innerHeight/1.5 - margin.top - margin.bottom;
+        w = window.innerWidth / 1.5 - margin.left - margin.right,
+        h = window.innerHeight / 1.5 - margin.top - margin.bottom;
 
 
     const doZoom = (e) => {
@@ -62,16 +60,17 @@
 
 
     // Mouse over : change color of path
-    const colors = [ "#fdf21f","#3fdf4b","#3462fe", "#fc0049", "#fe8f01"]
+    const colors = ["#fdf21f", "#3fdf4b", "#3462fe", "#fc0049", "#fe8f01"]
     let color = "#fdf21f";
 
     function handleMouseOver(e, canton_id) {
         selectAll("path").attr("stroke-width", 0);
         const selected_path = document.getElementById(canton_id)
-        color = colors[Math.floor(Math.random()*colors.length)]
+        color = colors[Math.floor(Math.random() * colors.length)]
         selected_path.style.stroke = color;
         selected_path.style.strokeWidth = 1.5;
     }
+
     function handleMouseOut(e, canton_id) {
         const selected_path = document.getElementById(canton_id)
         selected_path.style.strokeWidth = 0;
@@ -99,15 +98,15 @@
                 .domain([minX, maxX]);
 
             const y = scaleLinear()
-                .range([0, height /1.2 ])
+                .range([0, height / 1.2])
                 .domain([maxY, minY]);
 
-            const maxMariages = max(data.features.map(d => d.properties.total/ d.properties.pop * 1000))
+            const maxMariages = max(data.features.map(d => d.properties.total / d.properties.pop * 1000))
             const maxConversions = max(data.features.map(d => d.properties.total_c / d.properties.pop * 1000))
 
-            console.log(data.features.map(d=>d))
-            colorScaleMariage = scaleSequential(interpolate("white", "#802780")).domain([0, maxMariages])
-            colorScaleConversions = scaleSequential(interpolate("white", "#802780")).domain([0, maxConversions])
+            console.log(data.features.map(d => d))
+            colorScaleMariage = scaleSequential(interpolate("white", "#50c1a3")).domain([0, maxMariages])
+            colorScaleConversions = scaleSequential(interpolate("white", "#50c1a3")).domain([0, maxConversions])
 
 
             const projection = geoTransform({
@@ -122,9 +121,7 @@
             geodata = data.features
 
 
-
-
-         initZoom();
+            initZoom();
 
         }
     )
@@ -141,69 +138,85 @@
 
             <Column>
 
-    <div id="map">
-        <svg width={w} height={h}>
+                <div id="map">
+                    <svg width={w} height={h}>
 
-            <!--Cantons -->
-            {#key selected}
-            <g transition:fade>
-                {#each geodata as border, i}
-                    <g transform="translate({margin.left + margin.right},{margin.top + margin.bottom})">
-                    <path d={path(border)}
-                          fill="{selected == 'total' ? colorScaleMariage(border.properties[selected]/border.properties.pop*1000) : colorScaleConversions(border.properties[selected]/border.properties.pop*1000)}"
-                          on:mouseover={showTooltip(event, border.properties.canton)}
-                          on:mouseout={hideTooltip(event, border.properties.canton)}
-                          on:blur={hideTooltip(event, border.properties.canton)}
-                          on:focus={showTooltip(event, border.properties.canton)}
-                          id={"path-"+border.properties.canton}
-                          on:mouseenter={handleMouseOver(event,"path-"+border.properties.canton)}
-                          on:mouseleave={handleMouseOut(event,"path-"+border.properties.canton)}
-                        ></path>
-                    </g>
-                {/each}
-            </g>
-            {/key}
+                        <!--Cantons -->
+                        {#key selected}
+                            <g transition:fade>
+                                {#each geodata as border, i}
+                                    <g transform="translate({margin.left + margin.right},{margin.top + margin.bottom})">
+                                        <path d={path(border)}
+                                              fill="{selected == 'total' ? colorScaleMariage(border.properties[selected]/border.properties.pop*1000) : colorScaleConversions(border.properties[selected]/border.properties.pop*1000)}"
+                                              on:mouseover={showTooltip(event, border.properties.canton)}
+                                              on:mouseout={hideTooltip(event, border.properties.canton)}
+                                              on:blur={hideTooltip(event, border.properties.canton)}
+                                              on:focus={showTooltip(event, border.properties.canton)}
+                                              id={"path-"+border.properties.canton}
+                                              on:mouseenter={handleMouseOver(event,"path-"+border.properties.canton)}
+                                              on:mouseleave={handleMouseOut(event,"path-"+border.properties.canton)}
+                                        ></path>
+                                    </g>
+                                {/each}
+                            </g>
+                        {/key}
 
-        </svg>
+                    </svg>
 
 
-        {#each geodata as kanton, i}
-            <div id={kanton.properties.canton}
-                 style="position: absolute; display: none; color: #802780; background-color: whitesmoke; opacity: 0.6; padding: 20px; border-radius: 10%;">
-                <h5 style="color:{color}">{kanton.properties.canton}</h5>
-                Femme-Femme: <b>{kanton.properties.f_f}, conversion {kanton.properties.c_f_f} </b><br>
-                Homme-Homme: <b>{kanton.properties.h_h}, conversion {kanton.properties.c_h_h} </b><br>
-            </div>
+                    {#each geodata as kanton, i}
+                        <div id={kanton.properties.canton}
+                             style="position: absolute; display: none; background-color: whitesmoke; opacity: 0.6; padding: 20px; border-radius: 10%;">
+                            <h5 style="color:{color}">{kanton.properties.canton}</h5>
+                            Femmes: <b>{kanton.properties.f_f}, conversion {kanton.properties.c_f_f} </b><br>
+                            Hommes: <b>{kanton.properties.h_h}, conversion {kanton.properties.c_h_h} </b><br>
+                        </div>
 
-        {/each}
-    </div>
+                    {/each}
+                </div>
 
 
             </Column>
 
             <Column>
                 <div id="title">
-                <h2>Oui, je le veux 2022</h2>
+                    <h2>Oui, je le veux </h2>
+                    <br>
+                    <p>Le dimanche 26 septembre 2021, la Suisse a dit <strong>oui</strong> au mariage civil pour toutes et tous.
+                        Depuis juillet 2022, dans quel canton y a-t-il eu le plus de mariages du même sexe ? Et de conversions du partenariat enregistré ?
 
+                        <strong>Découvrons ensemble</strong> !</p>
                 </div>
+                <br>
+                <br>
                 <div id="labels">
-    <label>
-        <input checked={selected==='total'} on:change={onChange} type="radio" name="mariage" value="total" /> Mariages
-    </label>
+                    <label>
+                        <input checked={selected==='total'} on:change={onChange} type="radio" name="mariage"
+                               value="total"/> Mariages
+                    </label>
 
-    <label>
-        <input checked={selected==='total_c'} on:change={onChange} type="radio" name="mariage" value="total_c" /> Conversion partenariat enregistré
-    </label>
+                    <label>
+                        <input checked={selected==='total_c'} on:change={onChange} type="radio" name="mariage"
+                               value="total_c"/> Conversion partenariat enregistré
+                    </label>
                 </div>
+
+
             </Column>
         </Row>
     </Grid>
 
-    <footer><small><code>Données: <a href="https://www.bfs.admin.ch/bfs/fr/home/statistiques/population/mariages-partenaires-divorces/mariages.gnpdetail.2023-0216.html">Office fédéral de la statistique (OFS), Statistique du mouvement naturel de la
+    <footer><small><code>Données: <a
+            href="https://www.bfs.admin.ch/bfs/fr/home/statistiques/population/mariages-partenaires-divorces/mariages.gnpdetail.2023-0216.html">Office
+        fédéral de la statistique (OFS), Statistique du mouvement naturel de la
         population </a></code></small></footer>
 </main>
 
 <style>
+    #title {
+        padding-top: 10vh;
+    }
+    strong { font-weight: 1000; color: #50c1a3 }
     footer {
         text-align: right;
         right: 0;
@@ -212,34 +225,33 @@
         position: fixed;
     }
 
+    p {
+        color: #C7287D;
+    }
+
     label {
-       display: block;
-        color: #802780;
+        display: block;
         font-size: 1vw;
         line-height: 2vw;
     }
 
-    #labels{
-        margin-top: 30vh;
+    #labels {
+        color: #C7287D;
+
 
     }
 
     a {
-        color : #802780;
+        color: #C7287D;
         text-decoration: none;
     }
 
-    #title {
-        padding-top: 10vh;
-    }
 
     h2 {
-        color: ghostwhite;
+        color: #C7287D;
         font-family: "Roboto", sans-serif;
         text-transform: uppercase;
-        position: absolute;
-        font-size: 3vw;
-
+        font-size: 3.3vw;
         letter-spacing: 0.2em;
         cursor: pointer;
         text-shadow: 0.04em 0.04em #fc0049, 0.08em 0.08em #fe8f01,
